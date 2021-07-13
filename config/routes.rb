293794da1e2ce1dac996a4ddf795f6303a516c4a
Lillian <Lillianpparker@outlook.com>
2021-07-13 12:14:53 -0500

    all routes have been made

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root "homepage#home"
   get "/", to: "homepage#home"
   get "/resume", to: "resumetab#resume"
+  get "/bowerbird", to: "projectslist#bowerbird"
+  get "/faunafavor", to: "projectslist#faunafavor"
+  get "/herpkeeper", to: "projectslist#herpkeeper"
+  get "/contact", to: "contacttab#contact"
 end
